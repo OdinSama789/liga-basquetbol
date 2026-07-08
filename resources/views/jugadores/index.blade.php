d6c123df86c007abab219ebc4fbe538f65026bab
jose  julian <202213865h@utea.edu.pe>
2026-07-08 00:23:18 -0500

    Resuelve conflicto en jugadores

--- resources/views/jugadores/index.blade.php
+++ resources/views/jugadores/index.blade.php
@@ -10,79 +10,7 @@
     <a href="{{ route('jugadores.create') }}" class="btn btn-success">
         + Registrar Jugador
     </a>
-<<<<<<< HEAD
 
-    <div class="row">
-
-        @foreach($jugadores as $jugador)
-
-        <div class="col-md-4">
-
-            <div class="card mb-4 shadow">
-
-                <div class="card-body">
-
-                    <h3 class="card-title">
-                        {{ $jugador->nombre }}
-                    </h3>
-
-                    <p>
-                        <strong>Edad:</strong>
-                        {{ $jugador->edad }}
-                    </p>
-
-                    <p>
-                        <strong>Posición:</strong>
-                        {{ $jugador->posicion }}
-                    </p>
-
-                    <p>
-                        <strong>Equipo:</strong>
-                        {{ $jugador->equipo->nombre }}
-                    </p>
-
-                    <a href="{{ route('jugadores.show', ['jugador' => $jugador->id]) }}"
-                        class="btn btn-warning">
-
-                        Ver
-
-                    </a>
-
-                    <a href="{{ route('jugadores.edit', ['jugador' => $jugador->id]) }}"
-                        class="btn btn-primary">
-
-                        Editar
-
-                    </a>
-
-                    <form action="{{ route('jugadores.destroy', ['jugador' => $jugador->id]) }}"
-                        method="POST"
-                        class="d-inline">
-
-                        @csrf
-                        @method('DELETE')
-
-                        <button type="submit"
-                            class="btn btn-danger">
-
-                            Eliminar
-
-                        </button>
-
-                    </form>
-
-                </div>
-
-            </div>
-
-        </div>
-
-        @endforeach
-
-    </div>
-
-=======
->>>>>>> 9bf7f8e06ef0362cb47acc90cba7626aa66a9c20
 </div>
 
 <div class="card border-0 shadow-sm">
